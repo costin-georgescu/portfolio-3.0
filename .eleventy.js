b755b524facd0f705ac6435f4404d1423c849acb
Costin <130043906+costin-georgescu@users.noreply.github.com>
2025-07-24 17:25:14 +0300

    feat: add production environment config for Eleventy build and deployment

--- .eleventy.js
+++ .eleventy.js
@@ -53,8 +53,8 @@ module.exports = function(eleventyConfig) {
     templateFormats: ['html', 'njk', 'md', '11ty.js'],
     htmlTemplateEngine: 'njk',
     markdownTemplateEngine: 'njk',
-    markdownTemplateEngine: 'njk',
     dataTemplateEngine: 'njk',
-    passthroughFileCopy: true
+    passthroughFileCopy: true,
+    pathPrefix: process.env.ELEVENTY_ENV === 'PROD' ? '/portfolio-3.0/' : '/'
   };
 };
